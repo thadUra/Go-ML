e911a23c7fd6dbd0f16e405074b9258553361446
thaddziura <49488898+thaddziura@users.noreply.github.com>
2023-07-30 21:32:34 -0600

    Cleaned step function

--- game/env.go
+++ game/env.go
@@ -15,7 +15,7 @@ func InitEnvironment(args Soccer) Environment {
 
 	horizontal_angle := 90.0 * math.Pi / 180.0 // radians
 	vertical_angle := 90.0 * math.Pi / 180.0   // radians
-	power := 145.0                             // feet per second
+	power := 150.0                             // feet per second
 
 	var env Environment
 	env.args = args
@@ -34,7 +34,7 @@ func (env Environment) Step(action []float64, location Shot, print bool) int {
 	vertical_angle := action[1]
 	power := action[2]
 
-	// Calculate ball position along goal line
+	// Calculate ball position at goal line
 	gravity := -32.17
 	length := location.DISTANCE_Y / math.Cos(horizontal_angle)
 	velocity_x := math.Cos(vertical_angle) * power
@@ -44,37 +44,65 @@ func (env Environment) Step(action []float64, location Shot, print bool) int {
 	width := location.DISTANCE_X + (math.Sin(horizontal_angle) * length)
 
 	// Determine physics for bounce on ground (WIP)
+	rolling := false
 	coeff_restitution := 0.8
 	current_vel_y := velocity_y
-	goal_bounce_time := 0.0
-	if height < 0 && duration < 5 {
-		fmt.Println("===BOUNCE CALCULATIONS===")
-		bounceDuration := 0.0
-		count := 0
+	bounceDuration := 0.0
+	if height < 0 && duration < 7 {
+		if print {
+			fmt.Println("===BOUNCE CALCULATIONS===")
+		}
 		peak := env.args.BALL_DIAMETER + 1
+		goal_bounce_time := 0.0
 		for (bounceDuration < duration) && (peak > env.args.BALL_DIAMETER*0.6) {
 			time := (-1 * current_vel_y) / (0.5 * gravity)
+			peak = (current_vel_y * time / 2) + (0.5 * gravity * time * time / 4)
 			bounceDuration += time
 			if bounceDuration >= duration {
 				goal_bounce_time = bounceDuration - duration
 			}
-			peak = (current_vel_y * time / 2) + (0.5 * gravity * time * time / 4)
-			fmt.Printf("	Bounce %d with time: %f with peak: %f\n", count, bounceDuration, peak)
-			fmt.Printf("	Path Equation: (%ft+%f,(%ft - 16.085t^2)) %f\n", velocity_x, (bounceDuration-time)*velocity_x, current_vel_y, time)
+			if peak <= env.args.BALL_DIAMETER*0.6 {
+				rolling = true
+				height = env.args.BALL_DIAMETER / 2
+			}
+			if print {
+				fmt.Printf("	Path Equation: (%ft+%f,(%ft - 16.085t^2)) %f\n", velocity_x, (bounceDuration-time)*velocity_x, current_vel_y, time)
+			}
 			current_vel_y *= coeff_restitution
-			count++
 		}
 		bounceHeight := (current_vel_y * goal_bounce_time) + (0.5 * gravity * goal_bounce_time * goal_bounce_time)
-		fmt.Printf("	New height with bounces: %f\n", bounceHeight)
+		if print {
+			fmt.Printf("	New height with bounces: %f\n\n", bounceHeight)
+		}
 	}
 
 	// Determine physics for rolling (WIP)
+	roll_duration := 0.0
+	if rolling && bounceDuration < duration {
+		roll_distance := length - (velocity_x * bounceDuration)
+		roll_velocity_x := velocity_x
+		for roll_duration < 5 && roll_distance > 0 {
+			roll_distance -= roll_velocity_x
+			if roll_distance <= 0 {
+				roll_duration += (roll_distance + roll_velocity_x) / roll_velocity_x
+			}
+			roll_velocity_x *= 0.9
+			roll_duration++
+		}
+	}
+	if print {
+		fmt.Println("===DURATION CALCULATIONS===")
+		fmt.Printf("	Old Duration: %f\n", duration)
+		fmt.Printf("	New Duration: %f\n\n", roll_duration+bounceDuration)
+	}
+	duration = roll_duration + bounceDuration
 
-	// Determine dimensions for goal with Soccer struct params
+	// Determine parameters for goal
 	max_y := env.args.NET_HEIGHT - (env.args.BALL_DIAMETER / 2)
 	min_y := (env.args.BALL_DIAMETER / 2)
 	max_x := (env.args.FIELD_WIDTH / 2) + (env.args.NET_WIDTH / 2) - (env.args.BALL_DIAMETER / 2)
 	min_x := (env.args.FIELD_WIDTH / 2) - (env.args.NET_WIDTH / 2) + (env.args.BALL_DIAMETER / 2)
+	max_duration := 3.0
 
 	// Print statements
 	if print {
@@ -94,12 +122,20 @@ func (env Environment) Step(action []float64, location Shot, print bool) int {
 	}
 
 	// Determine reward (WIP)
-	// Include hitting left or right post + crossbar
-	if height >= min_y && height <= max_y && width >= min_x && width <= max_x {
-		fmt.Println("GOAL")
+	// Include hitting left or right post + crossbar and time to reach net
+	if height >= min_y &&
+		height <= max_y &&
+		width >= min_x &&
+		width <= max_x &&
+		duration < max_duration {
+		if print {
+			fmt.Println("GOAL")
+		}
 		return 10
 	} else {
-		fmt.Println("MISS")
+		if print {
+			fmt.Println("MISS")
+		}
 		return -20
 	}
 }
--- main.go
+++ main.go
@@ -4,72 +4,46 @@ import (
 	"Soccer-Penalty-Kick-ML-Threading/game"
 	"fmt"
 	"math"
+	"math/rand"
 	"sync"
+	"time"
 )
 
 var wg sync.WaitGroup
 
-// var count int
-var reward int
-var goals int
-
-// func addFull(start int, end int, thread bool) {
-// 	// var count int
-// 	for i := start; i < end; i++ {
-// 		count += 1
-// 	}
-// 	if thread {
-// 		defer wg.Done()
-// 	}
-// }
-
 func main() {
-
-	// Count Test No Threading
-	// start := time.Now()
-	// addFull(0, 1000000000, false)
-	// elapsed := time.Since(start)
-	// fmt.Printf("No Threading:%s\n", elapsed)
-
-	// Count Test Multithreading
-	// start = time.Now()
-	// wg.Add(2)
-	// go addFull(0, 500000000, true)
-	// go addFull(500000000, 1000000000, true)
-	// wg.Wait()
-	// elapsed = time.Since(start)
-	// fmt.Printf("Multithreading:%s\n", elapsed)
-
 	// Initialize game env
+	var reward int
+	var goals int
 	params := game.InitSoccer(0, 0, 0, 0, 0, 0, 0, true)
 	env := game.InitEnvironment(params)
-	// location := game.InitShot(0, 0, true)
+	location := game.InitShot(0, 0, true)
 
 	// Environment Space Tests
 
 	// Manual Test
-	location := game.InitShot(145, 50, false)
-	action := []float64{-25.0 * math.Pi / 180.0, 50.0 * math.Pi / 180.0, 35.0}
-	fmt.Printf("Reward: %d\n\n", env.Step(action, location, true))
+	// location := game.InitShot(145, 50, false)
+	// action := []float64{-25.0 * math.Pi / 180.0, 10.0 * math.Pi / 180.0, 35.0}
+	// fmt.Printf("Reward: %d\n\n", env.Step(action, location, true))
 
-	// 1000000 Random Episodes Timed No Threading
-	// test := func(action []float64) {
-	// 	result := env.Step(action, location, false)
-	// 	reward += result
-	// 	if result == 10 {
-	// 		goals++
-	// 	}
-	// }
-	// start := time.Now()
-	// for episode := 1; episode <= 100000000; episode++ {
-	// 	action := []float64{
-	// 		((rand.Float64() * 180) - 90) * math.Pi / 180.0,
-	// 		(rand.Float64() * 90) * math.Pi / 180.0,
-	// 		rand.Float64() * 145.0}
-	// 	test(action)
-	// }
-	// elapsed := time.Since(start)
-	// fmt.Printf("Multithreading: %s with reward: %d and %d goals\n", elapsed, reward, goals)
+	// 1000 Random Episodes Timed No Threading
+	test := func(action []float64) {
+		result := env.Step(action, location, false)
+		reward += result
+		if result == 10 {
+			goals++
+		}
+	}
+	start := time.Now()
+	for episode := 1; episode <= 1000; episode++ {
+		action := []float64{
+			((rand.Float64() * 180) - 90) * math.Pi / 180.0,
+			(rand.Float64() * 90) * math.Pi / 180.0,
+			rand.Float64() * 145.0}
+		test(action)
+	}
+	elapsed := time.Since(start)
+	fmt.Printf("Single Threaded: %s with reward: %d and %d goals\n", elapsed, reward, goals)
 
 	// 1000000 Random Episodes Timed Multithreading
 	// reward = 0
