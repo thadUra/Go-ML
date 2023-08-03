65204aa8f50f6102576d96f454ad17c182a6bc6c
thaddziura <49488898+thaddziura@users.noreply.github.com>
2023-08-03 14:21:51 -0600

    Added reference links for readme

--- soccer_ml.go
+++ soccer_ml.go
@@ -1,1 +1,11 @@
 package soccer_ml
+
+/**
+ * Links:
+ * https://towardsdatascience.com/math-neural-network-from-scratch-in-python-d6da9f29ce65
+ * https://github.com/nicknochnack/TensorflowKeras-ReinforcementLearning/blob/master/Deep%20Reinforcement%20Learning.ipynb
+ * https://hub.packtpub.com/build-reinforcement-learning-agent-in-keras-tutorial/
+ * https://github.com/keras-rl/keras-rl/blob/master/rl/policy.py
+ * https://medium.com/@qempsil0914/zero-to-one-deep-q-learning-part1-basic-introduction-and-implementation-bb7602b55a2c
+ * https://medium.com/artificialis/loss-functions-361b2ad439a0#:~:text=L2%20Loss%20(MSE)%20is%20more,and%20MSE%20would%20give%20100.
+ */
