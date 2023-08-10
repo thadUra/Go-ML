c409bbd5565974cd0c4a9421f160aabe35b2b782
thaddziura <49488898+thaddziura@users.noreply.github.com>
2023-08-10 12:33:24 -0600

    Added env package documentation

--- env/README.md
+++ env/README.md
@@ -1,3 +1,5 @@
-# Environment Package
+# Golang-Machine-Learning env
 
-WIP
+[![Documentation](https://img.shields.io/badge/documentation-GoDoc-blue.svg)](https://pkg.go.dev/github.com/thadUra/Golang-Machine-Learning/env)
+
+Package env is a package that mimics the gym library for reinforcement learning purposes. The library can be found at https://www.gymlibrary.dev/index.html.
--- env/environment.go
+++ env/environment.go
@@ -1,9 +1,9 @@
+// The env package contains code for generating a custom gym environment or to
+// replicate a similar environment from the gym library.
 package env
 
-/**
- *  Environment Interface
- *  Outlines necessary accessors and modifiers for environment
- */
+// Environment is a representation of a gym environment, which needs to implement the
+// Step, Reset, GetNumActions, and GetNumObservations functions.
 type Environment interface {
 	Step(action []float64) (float64, float64, bool, error)
 	Reset() float64
